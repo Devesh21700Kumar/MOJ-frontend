b199a5f53f832bb249110f3222af27845a6bd425
Mynk-9 <Mynk-9@users.noreply.github.com>
2020-12-27 03:12:43 +0530

    Linting fix

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ import {
 } from 'react-router-dom';
 import CoreDashboard from './components/core/CoreDashboard';
 import Personal from './personal/personal';
+import SendMessagePopup from './components/letterpopup/SendMessagePopup';
+import ReadMessagePopup from './components/letterpopup/ReadMessagePopup';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Personal />
+      {/* <SendMessagePopup
+        submitFunction={(a, b) => {
+          console.log(a, b);
+        }}
+      /> */}
+      <ReadMessagePopup messageArray={['This is a message.']} />
     </div>
   );
 }
