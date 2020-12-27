6839929485cc905a318999354bf55a6a1a951e08
Mynk-9 <Mynk-9@users.noreply.github.com>
2020-12-27 17:37:12 +0530

    Removed unnecessary imports
    Removed unnecessary imports from Read/SendMessagePopup

--- src/components/letterpopup/ReadMessagePopup.js
+++ src/components/letterpopup/ReadMessagePopup.js
@@ -1,9 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 
 import './LetterPopup.css';
 
--- src/components/letterpopup/SendMessagePopup.js
+++ src/components/letterpopup/SendMessagePopup.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
