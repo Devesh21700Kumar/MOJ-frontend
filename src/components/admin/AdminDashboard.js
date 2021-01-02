2d0d604e42fa7854908634a4b6227edc36923bcd
Rohit-Mundada <rohitrm0801@gmail.com>
2021-01-02 08:44:37 +0530

    Linting fix, Text on 0 messages, Router guards added, redirect to Login page on no token

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -58,6 +58,7 @@ const useStyles = makeStyles((theme) =>
       backgroundColor: '#FFFDE8',
       borderRadius: '20px 20px 0 0',
       padding: '1.5rem',
+      minHeight: '80vh',
     },
     flags: {
       width: '100%',
@@ -165,6 +166,12 @@ const useStyles = makeStyles((theme) =>
     backToTopButton: {
       textTransform: 'none',
       fontSize: '1rem',
+      fontFamily: 'Oxygen, sans serif',
+    },
+    noMessages: {
+      color: '#FC0404',
+      width: '100%',
+      textAlign: 'center',
       fontFamily: 'Oxygen, sans serif',
     },
     '@media(min-width: 320px)': {
@@ -309,10 +316,12 @@ const ShowMessages = ({
   messageId,
   setMessageId,
 }) => {
+  const classes = useStyles();
+
   return (
     <>
-      {redFlag === red
-        ? redFlaggedMsgs.length > 0 &&
+      {redFlag === red ? (
+        redFlaggedMsgs.length > 0 ? (
           redFlaggedMsgs.map((message, index) => (
             <MessageCard
               bitsId={message.receiverId}
@@ -326,8 +335,11 @@ const ShowMessages = ({
               n={setNumber()}
             />
           ))
-        : greenFlag === green
-        ? greenFlaggedMsgs.length > 0 &&
+        ) : (
+          <h1 className={classes.noMessages}>No Messages to Display!</h1>
+        )
+      ) : greenFlag === green ? (
+        greenFlaggedMsgs.length > 0 ? (
           greenFlaggedMsgs.map((message, index) => (
             <MessageCard
               bitsId={message.receiverId}
@@ -341,8 +353,11 @@ const ShowMessages = ({
               n={setNumber()}
             />
           ))
-        : yellowFlag === yellow
-        ? yellowFlaggedMsgs.length > 0 &&
+        ) : (
+          <h1 className={classes.noMessages}>No Messages to Display!</h1>
+        )
+      ) : yellowFlag === yellow ? (
+        yellowFlaggedMsgs.length > 0 ? (
           yellowFlaggedMsgs.map((message, index) => (
             <MessageCard
               bitsId={message.receiverId}
@@ -356,20 +371,26 @@ const ShowMessages = ({
               n={setNumber()}
             />
           ))
-        : msgs.length !== 0 &&
-          msgs.map((message, index) => (
-            <MessageCard
-              bitsId={message.receiverId}
-              body={message.body}
-              date={message.date}
-              key={index}
-              index={index}
-              _id={message._id}
-              messageId={messageId}
-              setMessageId={setMessageId}
-              n={setNumber()}
-            />
-          ))}
+        ) : (
+          <h1 className={classes.noMessages}>No Messages to Display!</h1>
+        )
+      ) : msgs.length !== 0 ? (
+        msgs.map((message, index) => (
+          <MessageCard
+            bitsId={message.receiverId}
+            body={message.body}
+            date={message.date}
+            key={index}
+            index={index}
+            _id={message._id}
+            messageId={messageId}
+            setMessageId={setMessageId}
+            n={setNumber()}
+          />
+        ))
+      ) : (
+        <h1 className={classes.noMessages}>No Messages to Display!</h1>
+      )}
     </>
   );
 };
@@ -392,34 +413,43 @@ const AdminDashboard = () => {
   const [snackBarOpen, setSnackBarOpen] = useState(false);
   const [type, setType] = useState('');
   const [i, seti] = useState(0);
+  const token = localStorage.getItem('token');
 
-  const { name, bitsId } = JSON.parse(
-    atob(localStorage.getItem('token').split('.')[1])
+  if (token === null) return <Redirect to="/" />;
+
+  const { permissionLevel, name, bitsId } = JSON.parse(
+    atob(token.split('.')[1])
   );
+
+  if (permissionLevel !== 2) return <Redirect to="/home" />;
 
   async function fetchMessages() {
     const { messages } = await (
       await fetch(`${URL}/api/level2/unassignedMessages`, {
         method: 'GET',
         headers: {
-          token: `${localStorage.getItem('token')}`,
+          token: `${token}`,
         },
       })
     ).json();
-    setMsgs(messages);
+    if (messages.length) {
+      setMsgs(messages.reverse());
+    }
 
     const { data } = await (
       await fetch(`${URL}/api/level2/finalapproval`, {
         method: 'GET',
         headers: {
-          token: `${localStorage.getItem('token')}`,
+          token: `${token}`,
         },
       })
     ).json();
     const { approved, denied, yellowflagged } = data;
-    setRedFlaggedMsgs(denied);
-    setYellowFlaggedMsgs(yellowflagged);
-    setGreenFlaggedMsgs(approved);
+    if (approved.length && denied.length && yellowflagged.length) {
+      setRedFlaggedMsgs(denied.reverse());
+      setYellowFlaggedMsgs(yellowflagged.reverse());
+      setGreenFlaggedMsgs(approved.reverse());
+    }
   }
 
   useEffect(() => {
@@ -613,7 +643,7 @@ const AdminDashboard = () => {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            token: `${localStorage.getItem('token')}`,
+            token: `${token}`,
           },
           body: JSON.stringify({ disapproveId: messageId }),
         })
@@ -636,7 +666,7 @@ const AdminDashboard = () => {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            token: `${localStorage.getItem('token')}`,
+            token: `${token}`,
           },
           body: JSON.stringify({ approveId: messageId }),
         })
