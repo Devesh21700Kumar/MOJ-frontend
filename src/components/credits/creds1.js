b8c6268dc7b75fe22ba9b3054e82cd8b7896eb81
Rohit-Mundada <rohitrm0801@gmail.com>
2021-01-22 17:07:57 +0530

    Credits page UI fix

--- src/components/credits/creds1.js
+++ src/components/credits/creds1.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Box, IconButton } from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
 import Navbar from '../navbar/navbar';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import '../credits/credits.css';
 import '../credits/creds1.css';
 import img1 from './../../imageassets/1A.jpeg';
 import img2 from './../../imageassets/2A.jpeg';
@@ -17,195 +18,54 @@ import img8 from './../../imageassets/3B.jpeg';
 import img9 from './../../imageassets/1C.jpeg';
 import img10 from './../../imageassets/2C.jpeg';
 import img11 from './../../imageassets/3C.jpeg';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     '@global': {
       '*::-webkit-scrollbar': {
-        width: '4px',
+        width: '10px',
+        backgroundColor: '#FFFDE8',
       },
       '*::-webkit-scrollbar-thumb': {
         backgroundColor: '#EF4646',
-        //borderRadius: '10px',
+        borderRight: '3px solid #FFFDE8',
+        borderLeft: '3px solid #FFFDE8',
       },
     },
     root: {
-      //position:'relative',
-      //borderRadius:'16px',
       backgroundColor: '#FFFDE8',
       flexGrow: 1,
       minHeight: '100vh',
-      //maxWidth: '100vw',
-      //padding: '1.5rem',
-      //height: '100vh',
       overflowX: 'hidden',
       overflowY: 'hidden',
       minHeight: '120vh',
-      //overflowY: 'scroll',
     },
     inner: {
       position: 'relative',
       width: '100%',
       minHeight: '100vh',
       backgroundColor: '#EF4646',
-      //overflowY:'scroll',
-      //minHeight:'100vh',
     },
     main: {
       padding: '1.5rem',
-      borderRadius: '20px 20px 0 0',
+      borderRadius: '10px 10px 0 0',
       backgroundColor: '#FFFDE8',
       overflowY: 'hidden',
       minHeight: '100vh',
     },
     mainContent: {
       zIndex: '1',
-      //borderRadius:'16px',
       overflowWrap: 'break-word',
       marginTop: '9.9vh',
       position: 'absolute',
       backgroundColor: '#FFFDE8',
       padding: '1.5rem',
       width: '40vw',
-      //minHeight: '79.5vh',
-    },
-    stats: {
-      overflowWrap: 'break-word',
-      width: '45vw',
-      margin: '2rem auto',
-    },
-    mainContentB: {
-      overflowY: 'hidden',
-      minHeight: '100vh',
-      //overflowWrap: 'break-word',
-      //minHeight:'110vh',
-      //overflowX:'hidden',
-      left: '50%',
-      marginTop: '4.9vh',
-      position: 'absolute',
-      //float:'right',
-      //Height:'100vh',
-      backgroundColor: '#FFFDE8',
-      padding: '1.5rem',
-      width: '50vw',
-    },
-    statsB: {
-      overflowWrap: 'break-word',
-      width: '85vw',
-      margin: '2rem auto',
-    },
-    stat: {
-      fontFamily: 'Oxygen, sans-serif',
-      fontSize: '18px',
-    },
-    stat1: {
-      fontFamily: 'Oxygen, sans-serif',
-      color: '#EF4646',
-      fontSize: '28px',
-    },
-    test: {
-      //zIndex: '12',
-      //position: 'fixed',
-      //width: '37vw',
-      position: 'relative',
-      overflow: 'hidden',
-      height: '14.5vmax',
-      width: '14.5vmax',
-      border: '2px solid #ef4646',
-      marginBottom: '3vh',
-      //left: '12.5vw',
-      //bottom: '30vh',
-      background: '#FFFFFF',
-    },
-    inactive: {
-      backgroundColor: '#4CBC14',
-      color: 'white',
-      padding: '10px 20px',
-      border: 'none',
-      cursor: 'pointer',
-      borderRadius: '30px',
-      boxShadow:
-        'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-      fontWeight: 'bold',
-      fontFamily: 'Oxygen',
-      outline: 'none',
-    },
-    inner: {
-      position: 'relative',
-      width: '100%',
-      minHeight: '100vh',
-      backgroundColor: '#EF4646',
-      //overflowY:'scroll',
-      //minHeight:'100vh',
-    },
-    inactiveOuter: {
-      borderRadius: '30px',
-    },
-    pad: {
-      paddingLeft: '2vw',
-    },
-
-    '@media(min-width: 540px)': {
-      stat: {
-        fontSize: '18px',
-      },
-    },
-    main: {
-      padding: '1.5rem',
-      borderRadius: '20px 20px 0 0',
-      backgroundColor: '#FFFDE8',
-      overflowY: 'hidden',
-      minHeight: '100vh',
     },
   })
 );
 
 const handlepics = (e) => {
-  //------------handle pics-----------------------
-  /*const container = document.querySelector('.photos');
-  const category = e.target.innerText;
-  const details = data.filter((obj) => obj.category == category)[0].details;
-  container.innerHTML = '';
-  details.map((obj) => {
-    //console.log(obj.pic);
-    const img = obj.pic;
-    //${obj.pic}.png
-    if (obj.post != '1') {
-      container.innerHTML += `
-			<div>
-			<div class="test">
-      <a href=${obj.post} target="_blank">
-      <img src=${img} alt="" />
-      </a>
-			</div>
-			<div class="details">
-				<p class="name">${obj.name}</p>
-        <p class="number">${obj.number}</p>
-        ${()=>{return(<div><IconButton><GitHubIcon/></IconButton><IconButton><LinkedInIcon/></IconButton><IconButton><InstagramIcon/></IconButton></div>)}}  
-			</div>
-		</div>
-    `;
-    } else {
-      container.innerHTML += `
-			<div>
-			<div class="test">
-      <img src=${img} alt="" />
-			</div>
-			<div class="details">
-				<p class="name">${obj.name}</p>
-        <p class="number">${obj.number}</p>
-        <div><IconButton><GitHubIcon/></IconButton><IconButton><LinkedInIcon/></IconButton><IconButton><InstagramIcon/></IconButton></div>
-			</div>
-		</div>
-    `;
-    }
-  });(*/
-
-  //--------------handle color of selected category----------
   const allCategories = [...document.querySelectorAll('.department')];
   allCategories.forEach((cat) => cat.classList.remove('selected'));
   e.target.classList.add('selected');
@@ -217,8 +77,6 @@ const Team = () => {
   const token = localStorage.getItem('token');
 
   if (token === null) return <Redirect to="/" />;
-
-  const [c1, setc1] = useState('photos1');
 
   const { name, bitsId } = JSON.parse(atob(token.split('.')[1]));
   return (
@@ -270,12 +128,9 @@ const Team = () => {
               <div className="photos">
                 {val == 0 ? (
                   <React.Fragment>
-                    <div>
+                    <div className="photo">
                       <div className="test">
-                        <img
-                          //src={'./../../imageassets/1.jpeg'}
-                          src={img9}
-                        />
+                        <img src={img9} className="image" />
                       </div>
                       <div className="details">
                         <p className="name">Ishant</p>
@@ -311,9 +166,9 @@ const Team = () => {
                         </div>
                       </div>
                     </div>
-                    <div>
+                    <div className="photo">
                       <div className="test">
-                        <img src={img11} />
+                        <img src={img11} className="image" />
                       </div>
                       <div className="details">
                         <p className="name">Samesh Lakhotia</p>
@@ -349,9 +204,9 @@ const Team = () => {
                         </div>
                       </div>
                     </div>
-                    <div>
+                    <div className="photo">
                       <div className="test">
-                        <img src={img10} />
+                        <img src={img10} className="image" />
                       </div>
                       <div className="details">
                         <p className="name">Arjun Bajpai</p>
@@ -390,16 +245,12 @@ const Team = () => {
                   </React.Fragment>
                 ) : val == 1 ? (
                   <React.Fragment>
-                    <div>
+                    <div className="photo">
                       <div className="test">
-                        <img
-                          //src={'./../../imageassets/1.jpeg'}
-                          src={img1}
-                        />
+                        <img src={img1} className="image" />
                       </div>
                       <div className="details">
                         <p className="name">Devesh Kumar</p>
-                        <p className="number">Lead Developer</p>
                         <div>
                           <IconButton>
                             <a
@@ -431,13 +282,12 @@ const Team = () => {
                         </div>
                       </div>
                     </div>
-                    <div>
+                    <div className="photo">
                       <div className="test">
-                        <img src={img2} />
+                        <img src={img2} className="image" />
                       </div>
                       <div className="details">
                         <p className="name">Rohit Rahul Mundada</p>
-                        <p className="number">Developer</p>
                         <div>
                           <IconButton>
                             <a
@@ -469,13 +319,12 @@ const Team = () => {
                         </div>
                       </div>
                     </div>
-                    <div>
+                    <div className="photo">
                       <div className="test">
-                        <img src={img3} />
+                        <img src={img3} className="image" />
                       </div>
                       <div className="details">
                         <p className="name">Aviral Kumar Goel</p>
-                        <p className="number">Developer</p>
                         <div>
                           <IconButton>
                             <a
@@ -507,13 +356,12 @@ const Team = () => {
                         </div>
                       </div>
                     </div>
-                    <div>
+                    <div className="photo">
                       <div className="test">
-                        <img src={img4} />
+                        <img src={img4} className="image" />
                       </div>
                       <div className="details">
                         <p className="name">Ritvij Kumar Sharma</p>
-                        <p className="number">Developer</p>
                         <div>
                           <IconButton>
                             <a
@@ -545,13 +393,12 @@ const Team = () => {
                         </div>
                       </div>
                     </div>
-                    <div>
+                    <div className="photo">
                       <div className="test">
-                        <img src={img5} />
+                        <img src={img5} className="image" />
                       </div>
                       <div className="details">
                         <p className="name">Mayank Mathur</p>
-                        <p className="number">Developer</p>
                         <div>
                           <IconButton>
                             <a
@@ -586,16 +433,12 @@ const Team = () => {
                   </React.Fragment>
                 ) : (
                   <React.Fragment>
-                    <div>
+                    <div className="photo">
                       <div className="test">
-                        <img
-                          //src={'./../../imageassets/1.jpeg'}
-                          src={img6}
-                        />
+                        <img src={img6} className="image" />
                       </div>
                       <div className="details">
                         <p className="name">Vedant Sachin Bang</p>
-                        <p className="number">Lead Developer</p>
                         <div>
                           <IconButton>
                             <a
@@ -616,18 +459,19 @@ const Team = () => {
                             </a>
                           </IconButton>
                           <IconButton>
-                            <InstagramIcon />
+                            <a style={{ color: '#8a3ab9' }} href="#">
+                              <InstagramIcon />
+                            </a>
                           </IconButton>
                         </div>
                       </div>
                     </div>
-                    <div>
+                    <div className="photo">
                       <div className="test">
-                        <img src={img7} />
+                        <img src={img7} className="image" />
                       </div>
                       <div className="details">
                         <p className="name">Vishnu Teja Soorea</p>
-                        <p className="number">Developer</p>
                         <div>
                           <IconButton>
                             <a
@@ -639,7 +483,9 @@ const Team = () => {
                             </a>
                           </IconButton>
                           <IconButton>
-                            <LinkedInIcon />
+                            <a style={{ color: '#0e76a8' }} href="#">
+                              <LinkedInIcon />
+                            </a>
                           </IconButton>
                           <IconButton>
                             <a
@@ -653,13 +499,12 @@ const Team = () => {
                         </div>
                       </div>
                     </div>
-                    <div>
+                    <div className="photo">
                       <div className="test">
-                        <img src={img8} />
+                        <img src={img8} className="image" />
                       </div>
                       <div className="details">
                         <p className="name">Gaurav Sharad Dotiya</p>
-                        <p className="number">Developer</p>
                         <div>
                           <IconButton>
                             <a
