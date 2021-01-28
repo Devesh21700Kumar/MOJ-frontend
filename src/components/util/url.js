7425375db222150bdabead1acf535b9f5dcd2238
Devesh <rockdev1234560@gmail.com>
2021-01-29 01:20:01 +0530

    colour and api call and fonts fixed

--- src/components/util/url.js
+++ src/components/util/url.js
@@ -1,4 +1,3 @@
-const url =
-  process.env.REACT_APP_BACKEND_URL || 'https://jogwbackend.herokuapp.com';
+const url = process.env.REACT_APP_BACKEND_URL || 'https://moj-nirmaan.tech';
 
 export default url;
