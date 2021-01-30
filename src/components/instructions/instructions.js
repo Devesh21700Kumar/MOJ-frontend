8254832eab34b1639ff17f759aaefd82186e8903
Devesh <rockdev1234560@gmail.com>
2021-01-30 18:53:53 +0530

    final commit

--- src/components/instructions/instructions.js
+++ src/components/instructions/instructions.js
@@ -175,9 +175,9 @@ const instructions = () => {
                     </a>
                     <br></br>
                     <b style={{ color: '#EF4646' }}>
-                      Ishant: +91 99969 41700
+                      Ishant : +91 99969 41700
                       <br></br>
-                      Samesh: +91 88925 40106
+                      Samesh : +91 88925 40106
                     </b>
                   </b>
                 </h3>
